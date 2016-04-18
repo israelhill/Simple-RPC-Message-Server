2a63c677536c5ff93fa7f2e8cef25cd653ab5cee
israel <idh@case.edu>
2016-04-18 15:05:51 -0400

    working on saving messages on server

--- server.c
+++ server.c
@@ -8,14 +8,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+client_data messages[100] = {{-1, ""}};
+
 //working
 int *put_1_svc(struct client_data *argp, struct svc_req *rqstp) {
 	static int  result;
 	int id = argp->client_id;
 //	char *msg = argp->client_msg;
 //	int msg = argp->client_msg;
-
-	result = printf("Server: %s, %d\n", argp->client_msg, id);
+	
+	
+	int i = 0;
+	while(messages[i] != {-1, ""}) {
+		messages[i] = *argp;
+	}
+//	messages[0] =* argp;
+	
+	
+	result = printf("Server: %s, %d\n", messages[0].client_msg, messages[0].client_id);
+//	result = printf("Server: %s, %d\n", argp->client_msg, id);
 //	result = printf("Server: %d, %d\n", msg, id);
 //	result = 99;
 	//result = printf("Server: Hello world");
