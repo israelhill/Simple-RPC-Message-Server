e3b173ffeaee28212149fd603db5cad524d7c9dc
Israel Hill <idh@case.edu>
2016-04-18 19:13:15 -0400

    dereferencing pointer

--- client.c
+++ client.c
@@ -51,7 +51,7 @@ void send_msgs(int id) {
     // send a message to the server
     return_value = put_1(&data, client);
 
-    if (return_value != -1) {
+    if (*return_value != -1) {
         printf("Client Says-- Put successful.\n");
     }
 }
@@ -63,7 +63,7 @@ void get_msgs() {
     sleep(5);
     return_value = get_1((void *) filler, client);
 
-    if (return_value == -1) {
+    if (*return_value == -1) {
         printf("Client Says-- There were no messages for me.\n");
     }
 }
